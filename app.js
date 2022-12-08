5ef611466b18f65bea7651b51a700bc33417b2c9
Chris Whitelam <cwhitelam70@gmail.com>
2022-12-07 20:10:48 -0500

    add error.js

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
+const { ifError } = require('assert');
+const { error } = require('console');
 
 
 mongoose.connect('mongodb://localhost:27017/chris-camp', {
@@ -48,6 +50,7 @@ app.get('/campgrounds/search', async (req, res) => {
 });
 
 app.post('/campgrounds', catchAsync(async (req, res, next) => {
+    if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground.id}`);
@@ -86,9 +89,9 @@ app.all('*', (req, res, next) => {
 })
 
 app.use((err, req, res, next) => {
-    const {statusCode = 500, message = 'Something went wrong'} = err;
-    res.status(statusCode).send(message);
-    res.send('Oh boy, something went wrong!')
+    const {statusCode = 500} = err;
+    if (!err.message) err.message = 'Oh No, Something went wrong'
+    res.status(statusCode).render('error', { err })
 })
 
 app.listen(3000, () => {
