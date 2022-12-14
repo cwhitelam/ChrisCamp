e07b489bc2d1d5d710f33bf587b060d78fd6051d
Chris Whitelam <cwhitelam70@gmail.com>
2022-12-13 20:08:47 -0500

    add schemas.js

--- app.js
+++ app.js
@@ -27,15 +27,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
 const validateCampground = (req, res, next) => {
-    const campgroundSchema = Joi.object({
-        campground: Joi.object({
-            title: Joi.string().required(),
-            price: Joi.number().required().min(0),
-            image: Joi.string().required(),
-            location: Joi.string().required(),
-            description: Joi.string().required()
-        }).required()
-    })
+    
     const result = campgroundSchema.validate(req.body);
     if (error) {
         const msg = error.details.map(el => el.message).join(',')
